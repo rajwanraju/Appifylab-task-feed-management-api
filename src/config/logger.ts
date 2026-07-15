ba72a40af8f97d9a9ef21eb9a6d1e5272c6a566d
rajwanraju <rajwan.diu@gmail.com>
2026-07-15 12:20:20 +0600

    pino updated for prod

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,18 +1,3 @@
 import pino from "pino";
 
-const isLocal =
-  process.env.NODE_ENV === "development" && !process.env.VERCEL;
-
-const logger = isLocal
-  ? pino(
-      pino.transport({
-        target: "pino-pretty",
-        options: {
-          colorize: true,
-          translateTime: "SYS:standard",
-        },
-      })
-    )
-  : pino();
-
-export default logger;
+export default pino();
